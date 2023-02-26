f6d753ab57da0b533330562f6b1c9f596d3db34d
koko <hcamargo03@misena.edu.co>
2023-02-26 03:50:21 -0500

    cqui tengo js con algunas cosas llego hasta last-child y e voy a dormir

--- js/script.js
+++ js/script.js
@@ -3,3 +3,29 @@ alert("Prueba tocando una delas pestañas <3")
 window.addEventList.toggle('scroll', funtion()
     .classList.toggle('active', window.scrollY >0)
 )
+
+// Esperar a que la página termine de cargarse
+document.addEventListener('DOMContentLoaded', function (){
+    // Adjunte el detector de eventos `change` al checkbox
+    document.getElementById('#billing-checkbox').onchange = toggleBilling;}, false);
+  
+  function toggleBilling() {
+    // Seleccione los campos de texto de facturación
+    var billingItems = document.querySelectorAll('#billing input[type="text"]');
+  
+    // Alternar los campos de texto de facturación
+    for (var i = 0; i < billingItems.length; i++) {
+      billingItems[i].disabled = !billingItems[i].disabled;
+    }
+  }
+
+  
+  document.querySelector("button").onclick = function () {
+    window.print();
+  }
+  var inputs = document.getElementsByTagName("input");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].indeterminate = true;
+}
+
--- pseudoclases/js/script.js
+++ pseudoclases/js/script.js
@@ -3,3 +3,35 @@ alert("prueba scrollear")
 window.addEventList.toggle('scroll', funtion()
     .classList.toggle('active', window.scrollY >0)
 )
+
+// Esperar a que la página termine de cargarse
+document.addEventListener('DOMContentLoaded', function (){
+    // Adjunte el detector de eventos `change` al checkbox
+    document.getElementById('#billing-checkbox').onchange = toggleBilling;}, false);
+  
+  function toggleBilling() {
+    // Seleccione los campos de texto de facturación
+    var billingItems = document.querySelectorAll('#billing input[type="text"]');
+  
+    // Alternar los campos de texto de facturación
+    for (var i = 0; i < billingItems.length; i++) {
+      billingItems[i].disabled = !billingItems[i].disabled;
+    }
+  }
+
+  
+  document.querySelectorAll("button").onclick = function() {
+    window.print();
+  }
+
+
+  var inputs = document.getElementsByTagName("input");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].indeterminate = true;
+}
+
+
+
+  
+ 
